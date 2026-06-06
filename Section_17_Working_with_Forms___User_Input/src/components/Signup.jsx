74e6fac810259eaa8fd0e143aeb473e8434c7821
Microwave2000 <nagyzsadam@gmail.com>
2026-06-06 19:45:08 +0200

    Confirm password validation logic added

--- Section_17_Working_with_Forms___User_Input/src/components/Signup.jsx
+++ Section_17_Working_with_Forms___User_Input/src/components/Signup.jsx
@@ -1,4 +1,8 @@
+import { useState } from "react";
+
 export default function Signup() {
+  const [passwordsAreNotEqual, setPasswordsAreNotEqual] = useState(false);
+
   function handleSubmit(event) {
     event.preventDefault();
 
@@ -6,8 +10,13 @@ export default function Signup() {
     const acquisitionChannel = fd.getAll("acquisition");
     const data = Object.fromEntries(fd.entries());
     data.acquisition = acquisitionChannel;
-    console.log(data);
 
+    if (data.password !== data["confirm-password"]) {
+      setPasswordsAreNotEqual(true);
+      return;
+    }
+
+    console.log(data);
     //event.target.reset()
   }
   return (
@@ -40,6 +49,9 @@ export default function Signup() {
             name="confirm-password"
             required
           />
+          <div className="control-error">
+            {passwordsAreNotEqual && <p>Passwords must match.</p>}
+          </div>
         </div>
       </div>
 
