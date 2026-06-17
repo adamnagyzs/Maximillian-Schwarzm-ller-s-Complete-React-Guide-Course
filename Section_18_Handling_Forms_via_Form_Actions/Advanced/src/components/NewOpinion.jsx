2f5e4bf3d51a0b8ecd2bcb0c6896f6b257266ea5
Microwave2000 <nagyzsadam@gmail.com>
2026-06-17 16:05:11 +0200

    Sending formData to backend (submitting)

--- Section_18_Handling_Forms_via_Form_Actions/Advanced/src/components/NewOpinion.jsx
+++ Section_18_Handling_Forms_via_Form_Actions/Advanced/src/components/NewOpinion.jsx
@@ -1,42 +1,48 @@
-import { useActionState } from "react";
-
-function shareOpinionAction(prevState, formData) {
-  const title = formData.get("title");
-  const body = formData.get("body");
-  const userName = formData.get("userName");
-
-  const errors = [];
-
-  if (title.trim().length < 5) {
-    errors.push("Title must be at least five characters long.");
-  }
-
-  if (body.trim().length < 10 || body.trim().length > 300) {
-    errors.push("Opinion must be between 10 and 300 characters long.");
-  }
-
-  if (!userName.trim()) {
-    errors.push("Please provide your name.");
-  }
-
-  if (errors.length > 0) {
-    return {
-      errors,
-      enteredValues: {
-        title,
-        body,
-        userName,
-      },
-    };
-  }
-
-  return { errors: null };
-}
+import { useActionState, use } from "react";
+import { OpinionsContext } from "../store/opinions-context";
 
 export function NewOpinion() {
+  const { addOpinion } = use(OpinionsContext);
+
+  async function shareOpinionAction(prevState, formData) {
+    const title = formData.get("title");
+    const body = formData.get("body");
+    const userName = formData.get("userName");
+
+    const errors = [];
+
+    if (title.trim().length < 5) {
+      errors.push("Title must be at least five characters long.");
+    }
+
+    if (body.trim().length < 10 || body.trim().length > 300) {
+      errors.push("Opinion must be between 10 and 300 characters long.");
+    }
+
+    if (!userName.trim()) {
+      errors.push("Please provide your name.");
+    }
+
+    if (errors.length > 0) {
+      return {
+        errors,
+        enteredValues: {
+          title,
+          body,
+          userName,
+        },
+      };
+    }
+
+    await addOpinion({ title, body, userName });
+
+    return { errors: null };
+  }
+
   const [formState, formAction] = useActionState(shareOpinionAction, {
     errors: null,
   });
+
   return (
     <div id="new-opinion">
       <h2>Share your opinion!</h2>
