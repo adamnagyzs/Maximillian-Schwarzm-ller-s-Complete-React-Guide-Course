edf6f1b83fe5a22cb051b829dfd095c9d5ea637d
Microwave2000 <nagyzsadam@gmail.com>
2026-06-18 20:30:40 +0200

    outputting number of orders

--- Section_19_Practice_Project_Food_Order_App/src/components/Header.jsx
+++ Section_19_Practice_Project_Food_Order_App/src/components/Header.jsx
@@ -1,7 +1,15 @@
+import { useContext } from "react";
+
 import Button from "./UI/Button";
 import logoImg from "../assets/logo.jpg";
+import CartContext from "../store/CartContext";
 
 export default function Header() {
+  const cartCtx = useContext(CartContext);
+
+  const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
+    return totalNumberOfItems + item.quantity;
+  }, 0);
   return (
     <header id="main-header">
       <div id="title">
@@ -10,7 +18,7 @@ export default function Header() {
       </div>
       <p></p>
       <nav>
-        <Button textOnly={true}>Cart (0)</Button>
+        <Button textOnly={true}>Cart ({totalCartItems})</Button>
       </nav>
     </header>
   );
