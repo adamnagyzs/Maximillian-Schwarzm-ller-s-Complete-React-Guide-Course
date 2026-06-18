578efdb75acc9e4b794cf12ab9da9cc1f4b84223
Microwave2000 <nagyzsadam@gmail.com>
2026-06-18 20:29:18 +0200

    removing items work (finished context)

--- Section_19_Practice_Project_Food_Order_App/src/store/CartContext.jsx
+++ Section_19_Practice_Project_Food_Order_App/src/store/CartContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useReducer } from "react";
 const CartContext = createContext({
   items: [],
   addItem: (item) => {},
-  removeItem: (id = {}),
+  removeItem: (id) => {},
 });
 
 function cartReducer(state, action) {
@@ -19,7 +19,7 @@ function cartReducer(state, action) {
       const existingItem = state.items[existingCartItemIndex];
       const updatedItem = {
         ...existingItem,
-        quantity: existingItem + 1,
+        quantity: existingItem.quantity + 1,
       };
 
       updatedItems[existingCartItemIndex] = updatedItem;
@@ -30,15 +30,49 @@ function cartReducer(state, action) {
     return { ...state, items: updatedItems };
   }
   if (action.type === "REMOVE_ITEM") {
-  }
+    const existingCartItemIndex = state.items.findIndex(
+      // returns -1 if not found
+      (item) => item.id === action.id,
+    );
+    const existingCartItem = state.items[existingCartItemIndex];
 
+    const updatedItems = [...state.items];
+
+    if (existingCartItem.quantity === 1) {
+      updatedItems.splice(existingCartItemIndex, 1);
+    } else {
+      const updatedItem = {
+        ...existingCartItem,
+        quantity: existingCartItem.quantity - 1,
+      };
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
+    return { ...state, items: updatedItems };
+  }
   return state;
 }
 
 export function CartContextProvider({ children }) {
-  useReducer(cartReducer, { items: [] });
+  const [cart, dispatchCartAction] = useReducer(cartReducer, { items: [] });
 
-  return <CartContext.Provider>{children}</CartContext.Provider>;
+  function addItem(item) {
+    dispatchCartAction({ type: "ADD_ITEM", item });
+  }
+
+  function removeItem(id) {
+    dispatchCartAction({ type: "REMOVE_ITEM", id });
+  }
+
+  const cartContext = {
+    items: cart.items,
+    addItem,
+    removeItem,
+  };
+
+  return (
+    <CartContext.Provider value={cartContext}>{children}</CartContext.Provider>
+  );
 }
 
 export default CartContext;
