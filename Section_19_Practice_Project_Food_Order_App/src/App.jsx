a22ed48fc9a67cbdddabf347dc1b8713d5ba3f1e
Microwave2000 <nagyzsadam@gmail.com>
2026-06-18 20:30:07 +0200

    wrapping app with cart context

--- Section_19_Practice_Project_Food_Order_App/src/App.jsx
+++ Section_19_Practice_Project_Food_Order_App/src/App.jsx
@@ -1,12 +1,13 @@
+import { CartContextProvider } from "./store/CartContext";
 import Header from "./components/Header";
 import Meals from "./components/Meals";
 
 function App() {
   return (
-    <>
+    <CartContextProvider>
       <Header />
       <Meals />
-    </>
+    </CartContextProvider>
   );
 }
 
