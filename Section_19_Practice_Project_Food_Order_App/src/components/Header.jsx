81d9d2afb015f2ac438ff57e046125ee3d25a7d4
Microwave2000 <nagyzsadam@gmail.com>
2026-06-19 17:06:30 +0200

    opening cart modal works

--- Section_19_Practice_Project_Food_Order_App/src/components/Header.jsx
+++ Section_19_Practice_Project_Food_Order_App/src/components/Header.jsx
@@ -3,13 +3,20 @@ import { useContext } from "react";
 import Button from "./UI/Button";
 import logoImg from "../assets/logo.jpg";
 import CartContext from "../store/CartContext";
+import UserProgressContext from "../store/UserProgressContext";
 
 export default function Header() {
   const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
 
   const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
     return totalNumberOfItems + item.quantity;
   }, 0);
+
+  function handleShowCart() {
+    userProgressCtx.showCart();
+  }
+
   return (
     <header id="main-header">
       <div id="title">
@@ -18,7 +25,9 @@ export default function Header() {
       </div>
       <p></p>
       <nav>
-        <Button textOnly={true}>Cart ({totalCartItems})</Button>
+        <Button textOnly onClick={handleShowCart}>
+          Cart ({totalCartItems})
+        </Button>
       </nav>
     </header>
   );
