775577d1bfdcb90844d033b60a673768fe726dec
Microwave2000 <nagyzsadam@gmail.com>
2026-06-24 18:28:17 +0200

    toggle button works

--- Section_20_Diving_into_Redux/redux_project/src/store/index.js
+++ Section_20_Diving_into_Redux/redux_project/src/store/index.js
@@ -1,21 +1,35 @@
 import { createStore } from "redux";
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialState = { counter: 0, showCounter: true };
+
+const counterReducer = (state = initialState, action) => {
+  // the state objects which we return will OVERRIDE our existing state
+  // IMPORTANT: never change the existing state, but always OVERRIDE it
   if (action.type === "increment") {
     return {
       counter: state.counter + 1,
+      showCounter: state.showCounter,
     };
   }
 
   if (action.type === "increase") {
     return {
       counter: state.counter + action.amount,
+      showCounter: state.showCounter,
     };
   }
 
   if (action.type === "decrement") {
     return {
       counter: state.counter - 1,
+      showCounter: state.showCounter,
+    };
+  }
+
+  if (action.type === "toggle") {
+    return {
+      showCounter: !state.showCounter,
+      counter: state.counter,
     };
   }
 
