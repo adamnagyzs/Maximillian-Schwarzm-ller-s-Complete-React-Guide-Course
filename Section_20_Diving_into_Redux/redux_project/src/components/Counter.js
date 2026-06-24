cd0c79d2efe262dd954baee372638a46b99cc324
Microwave2000 <nagyzsadam@gmail.com>
2026-06-24 19:29:09 +0200

    toggle counter button fixed

--- Section_20_Diving_into_Redux/redux_project/src/components/Counter.js
+++ Section_20_Diving_into_Redux/redux_project/src/components/Counter.js
@@ -27,14 +27,12 @@ const Counter = () => {
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
-      {show && (
-        <div>
-          <button onClick={incrementHandler}>Increment</button>
-          <button onClick={increaseHandler}>Increase by 5</button>
-          <button onClick={decrementHandler}>Decrement</button>
-        </div>
-      )}
+      {show && <div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 5</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
