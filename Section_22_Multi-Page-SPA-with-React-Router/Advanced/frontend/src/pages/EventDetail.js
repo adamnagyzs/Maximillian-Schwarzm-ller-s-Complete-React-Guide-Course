ae32e887d17013b6fd0f9343cebef03f3ac0824e
Microwave2000 <nagyzsadam@gmail.com>
2026-07-01 19:48:29 +0200

    event details fetched

--- Section_22_Multi-Page-SPA-with-React-Router/Advanced/frontend/src/pages/EventDetail.js
+++ Section_22_Multi-Page-SPA-with-React-Router/Advanced/frontend/src/pages/EventDetail.js
@@ -1,18 +1,27 @@
-import { useParams, Link } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
+
+import EventItem from "../components/EventItem";
 
 function EventDetailPage() {
-  const params = useParams();
-  return (
-    <>
-      <h1>EventDetailPage</h1>
-      <p>Event ID: {params.eventId}</p>
-      <p>
-        <Link to=".." relative="path">
-          Back
-        </Link>
-      </p>
-    </>
-  );
+  const data = useLoaderData();
+
+  return <EventItem event={data.event} />;
 }
 
 export default EventDetailPage;
+
+export async function loader({ request, params }) {
+  const id = params.eventId;
+  const response = await fetch("http://localhost:8080/events/" + id);
+
+  if (!response.ok) {
+    throw new Error(
+      JSON.stringify({
+        message: "Could not fetch details for selected event.",
+      }),
+      { status: 500 },
+    );
+  } else {
+    return response;
+  }
+}
