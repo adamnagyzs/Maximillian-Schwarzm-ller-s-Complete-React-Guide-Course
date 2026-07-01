f634a04e2cbcb004a804c7dc565f2caf35740d1f
Microwave2000 <nagyzsadam@gmail.com>
2026-07-01 18:44:58 +0200

    loader function infos

--- Section_22_Multi-Page-SPA-with-React-Router/Advanced/frontend/src/pages/Events.js
+++ Section_22_Multi-Page-SPA-with-React-Router/Advanced/frontend/src/pages/Events.js
@@ -3,7 +3,9 @@ import { useLoaderData } from "react-router-dom";
 import EventsList from "../components/EventsList";
 
 function EventsPage() {
-  const events = useLoaderData();
+  const data = useLoaderData();
+  const events = data.events;
+
   return (
     <>
       <EventsList events={events} />
@@ -14,12 +16,15 @@ function EventsPage() {
 export default EventsPage;
 
 export async function loader() {
+  // this executes in the browser, not in backend server
+  // you can't use hooks here, because it isn't a react component, but any other browser features can be used
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
     // ...
   } else {
-    const resData = await response.json();
-    return resData.events;
+    // const resData = await response.json();
+    // return resData.events;
+    return response; // you can do it like this with useLoaderData()
   }
 }
