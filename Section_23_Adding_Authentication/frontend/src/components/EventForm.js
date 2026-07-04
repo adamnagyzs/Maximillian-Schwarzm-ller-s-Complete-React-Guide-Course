90f421ccd907e0165f742038f576baa1fd82f03b
Microwave2000 <nagyzsadam@gmail.com>
2026-07-04 14:47:13 +0200

    new & editing event requires auth

--- Section_23_Adding_Authentication/frontend/src/components/EventForm.js
+++ Section_23_Adding_Authentication/frontend/src/components/EventForm.js
@@ -3,11 +3,11 @@ import {
   useNavigate,
   useNavigation,
   useActionData,
-  json,
   redirect,
 } from "react-router-dom";
 
 import classes from "./EventForm.module.css";
+import { getAuthToken } from "../util/auth";
 
 function EventForm({ method, event }) {
   const data = useActionData();
@@ -101,10 +101,13 @@ export async function action({ request, params }) {
     url = "http://localhost:8080/events/" + eventId;
   }
 
+  const token = getAuthToken();
+
   const response = await fetch(url, {
     method: method,
     headers: {
       "Content-Type": "application/json",
+      Authorization: "Bearer " + token,
     },
     body: JSON.stringify(eventData),
   });
